9f3ea337f9b5c04cd23c60755a4a6ba73e427cde
gayatri <venkat.b@mail.com>
2016-12-18 15:13:31 -0500

    Message java file added

---
+++ Message.java
@@ -0,0 +1,17 @@
+public class Message {
+public static void main(String[] s){
+
+
+if(s[0].equals("ram")){
+System.out.println("God in human");
+}
+else if(s[0].equals("gayatri")){
+System.out.println("God of knowledge");
+}else {
+    	System.out.println("No Information Available");
+		}
+		
+System.out.println("End of the execution");
+}
+}
+
