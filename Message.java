d447c158100b3824627d242fda1f90f2a5b82ad0
gayatri <venkat.b@mail.com>
2016-12-21 00:40:01 -0500

    Message modified

--- Message.java
+++ Message.java
@@ -1,11 +1,11 @@
 public class Message {
 public static void main(String[] s){
+String[] s1={"ram","gayatri","sam"};
 
-
-if(s[0].equals("ram")){
+if(s1[0].equals("ram")){
 System.out.println("God in human");
 }
-else if(s[0].equals("gayatri")){
+else if(s[1].equals("gayatri")){
 System.out.println("God of knowledge");
 }else {
     	System.out.println("No Information Available");
